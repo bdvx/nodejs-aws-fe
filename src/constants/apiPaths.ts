4a3aee234a6a10260cefa51ef05a2429885a7f23
Denis Beskorovainyi <74551976+DBeskorovainyiEPAM@users.noreply.github.com>
2024-07-13 03:01:38 +0400

    feat: adjust fe 1

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   products: 'https://5naewp4kya.execute-api.eu-west-1.amazonaws.com/prod',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://vlcuky66lj.execute-api.eu-west-1.amazonaws.com/prod',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
